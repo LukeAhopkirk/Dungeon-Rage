464aa05f689d3ba947d996657df9914e7c26fd90
Nick Garner <garni988@student.otago.ac.nz>
2024-02-02 13:51:25 +1300

    Added ragebar and xp bar, both fill with method calls

--- DungeonRage/Assets/Scripts/HUDManager.cs
+++ DungeonRage/Assets/Scripts/HUDManager.cs
@@ -7,18 +7,24 @@ public class HUDManager : MonoBehaviour
 {
 
     public Image healthBar;
+    public Image experienceBar;
+    public Image rageBar;
     public Button[] spellButtons;
     public float healthAmount = 100f;
+    public float experienceAmount = 0f;
+    public float rageAmount = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        healthBar.fillAmount = healthAmount / 100f;
+        experienceBar.fillAmount = experienceAmount / 100f;
+        rageBar.fillAmount = rageAmount / 100f;
     }
 
     private void Update()
     {
-        
+
     }
 
     public void TakeDamage(float damage)
@@ -33,5 +39,20 @@ public class HUDManager : MonoBehaviour
         healthAmount = Mathf.Clamp(healthAmount, 0f, 100f);
 
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void GetExperience(float experience)
+    {
+        experienceAmount += experience;
+        experienceAmount = Mathf.Clamp(experienceAmount, 0f, 100f);
+
+        experienceBar.fillAmount = experienceAmount / 100f;
+    }
+    public void GetRage(float rage)
+    {
+        rageAmount += rage;
+        rageAmount = Mathf.Clamp(rageAmount, 0f, 100f);
+
+        rageBar.fillAmount = rageAmount / 100f;
     }
 }
