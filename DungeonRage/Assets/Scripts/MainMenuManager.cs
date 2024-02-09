c6c734986ff1ab8b33a76496b64e6871c01e91a6
Nick Garner <garni988@student.otago.ac.nz>
2024-02-10 01:39:53 +1300

    Main menu - options menu

---
+++ DungeonRage/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tut+Level1");
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }   
+}
