ef1573869491390961aee24a24c33f560da8edcd
Nick Garner <garni988@student.otago.ac.nz>
2024-02-10 16:49:01 +1300

    finished floating text prefab

--- DungeonRage/Assets/FloatingText.cs
+++ DungeonRage/Assets/FloatingText.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     public float DestroyTime = 3f;
+    public Vector3 Offset = new Vector3(0, 0, 0);
+    public Vector3 RandomizeIntensity = new Vector3(0.5f, 0, 0);
 
     private void Start()
     {
         Destroy(gameObject, DestroyTime);
+        transform.localPosition += Offset;
+        transform.localPosition += new Vector3(Random.Range(-RandomizeIntensity.x, RandomizeIntensity.x),
+        Random.Range(-RandomizeIntensity.y, RandomizeIntensity.y),
+        Random.Range(-RandomizeIntensity.z, RandomizeIntensity.z));
     }
 
-    private void Update()
-    {
-        
-    }
 }
